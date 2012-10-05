23122ae4a3e6f92612d1f13fc752bab681ea3a00
Lev Dashevskiy <ldashevs@fhcrc.org>
2012-10-04 17:57:39 -0700

    Fixes to the writeProjections() method.

--- src/queries/exp/XmlFiles.sql
+++ src/queries/exp/XmlFiles.sql
@@ -1,5 +1,7 @@
 SELECT DISTINCT
   DataInputs.Data.Name AS FileName,
   DataInputs.Data.DataFileUrl AS FilePath
-FROM DataInputs
-WHERE DataInputs.Role = 'Workspace'
+FROM
+ DataInputs
+WHERE
+ DataInputs.Role = 'Workspace'
--- src/reports/schemas/OpenCytoPreprocessing.r
+++ src/reports/schemas/OpenCytoPreprocessing.r
@@ -4,7 +4,7 @@ suppressMessages( library(Rlabkey) );
 path <- labkey.url.params$path;
 name <- labkey.url.params$name;
 
-if ( path != '' & name!= '' ){
+if ( path != '' & name!= '' ) {
 
     folderPath <- unlist( strsplit( path, split = '/' ) );
     len <- length( folderPath ) - 1;
@@ -18,43 +18,47 @@ if ( path != '' & name!= '' ){
         print('Before parsing (unarchiving)');
     
         suppressMessages( ws <- openWorkspace( path ) );
-#        G <- parseWorkspace( ws, name = name );
-        G <- suppressMessages( unarchive( paste( folderPath, '/GatingSet.tar', sep = '' ) ) );
+        G <- parseWorkspace( ws, name = name );
+#        G <- suppressMessages( unarchive( paste( folderPath, '/GatingSet.tar', sep = '' ) ) );
 
-    writeProjections <- function(G,...){
-        gh <- G[[1]];
-        popNames <- getNodes( gh, isPath = T )
-        nodeNames <- getNodes( gh )
-        res <- lapply( 1:length(nodeNames), function(i) {
-                    curPop <- popNames[i]
-                    curpNode <- nodeNames[i]
+        writeProjections <- function(G,...){
+            gh <- G[[1]];
+            popNames <- getNodes( gh, isPath = T );
+            nodeNames <- getNodes( gh );
+            res <- lapply( 1:length(nodeNames), function(i) {
+                curPop <- popNames[i];
+                curpNode <- nodeNames[i];
 
-                    #store the children gate projections
-                    curChildrens <- getChildren(gh,curpNode)
-                    if( length( curChildrens ) > 0 )
-                    {
-                        prjlist <- lapply( curChildrens, function(curChildren) {
-                                    g <- getGate( gh, curChildren )
-                                    print(g)
-                                    if ( class(g) == "BooleanGate" ){
-                                        return( NULL );
-                                    }else {
-                                        return( parameters(g) );
-                                    }
-                                })
-                        prj <- do.call(rbind, prjlist)
-                        prj <- unique(prj)
-                        prj <- as.data.frame(prj)
-                        colnames(prj) <- c('x_axis', 'y_axis')
-                        cbind( name = curpNode, path = curPop, prj )
-                    } else {
+                #store the children gate projections
+                curChildrens <- getChildren( gh, curpNode );
+                if ( length( curChildrens ) > 0 ) {
+                    prjlist <- lapply( curChildrens, function(curChildren) {
+                        g <- getGate( gh, curChildren );
+                        if ( class(g) == "BooleanGate" ){
+                            return( NULL );
+                        } else {
+                            param <- parameters( g );
 
-                    }
-                })
+                            if ( length( param ) == 1 ){
+                                param<-c(param,"SSC-A");
+                            }
+                            return( param );
+                        }
+                    })
+                    prj <- do.call(rbind, prjlist)
+                    prj <- unique(prj)
+                    prj <- as.data.frame(prj)
 
-        toInsert <- do.call( rbind, res )
-        insertedRow <- labkey.insertRows( queryName = 'projections', toInsert = toInsert, ...)
-    };
+                    colnames(prj) <- c('x_axis', 'y_axis')
+                    cbind( name = curpNode, path = curPop, prj )
+                } else {
+                    print( 'not yet implemented' );
+                }
+            });
+
+            toInsert <- do.call( rbind, res )
+#            insertedRow <- labkey.insertRows( queryName = 'projections', toInsert = toInsert, ...)
+        };
 
         print('Before writing projections');
         writeProjections( G, baseUrl = labkey.url.base, folderPath = labkey.url.path, schemaName = 'opencyto_preprocessing' );
@@ -64,7 +68,7 @@ if ( path != '' & name!= '' ){
         colnames(meta)[1] <- 'name';
         pData(G) <- meta;
 
-#        archive( G, gatingSetPath );
+        archive( G, gatingSetPath );
 
     }
 
